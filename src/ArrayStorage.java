34726ae8352f27919890e02c1cb4ca7361c5fbd8
GlyzinAI <glizin.art@gmail.com>
2019-03-22 12:18:50 +0300

    HW1. ArrayStorage implementation.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,94 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
+ *
+ * @author Artur Glyzin.
+ * @version 1.0.
+ * @since 22.03.2019.
  */
 public class ArrayStorage {
+    private int index;
+    private int size;
+
+    /**
+     * Resume storage.
+     */
+
     Resume[] storage = new Resume[10000];
 
+    /**
+     * Clear storage
+     */
+
     void clear() {
+
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
+
+    /**
+     * Add resume to storage.
+     *
+     * @param r - resume.
+     */
 
     void save(Resume r) {
+        this.storage[index++] = r;
+        size++;
     }
+
+    /**
+     * Returns a resume.
+     *
+     * @param uuid - uuid resume.
+     * @return Resume.
+     */
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = new Resume();
+        resume.uuid = "В базе нет резюме с таким uuid, пожалуйста введите корректный uuid.";
+        for (Resume r : getAll()) {
+            if (r.uuid.equals(uuid)) {
+                resume = r;
+                break;
+            }
+        }
+        return resume;
     }
 
+    /**
+     * Remove resume from storage.
+     *
+     * @param uuid
+     */
+
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - i);
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
+    /**
+     * Returns the number of elements.
+     *
+     * @return size.
+     */
+
     int size() {
-        return 0;
+        return this.size;
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -1,5 +1,9 @@
 /**
  * Initial resume class
+ *
+ * @author Artur Glyzin.
+ * @version 1.0.
+ * @since 22.03.2019.
  */
 public class Resume {
 
