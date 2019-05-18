ddf0518fda3123c67f1c997e101e4b464fc53568
GlyzinAI <glizin.art@gmail.com>
2019-05-18 23:53:14 +0300

    Lesson#16 HTML Table + Servlet

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,14 +1,27 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
+import com.urise.webapp.model.ContactType;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.Storage;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
@@ -17,36 +30,31 @@ public class ResumeServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        /*String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello, " + name + "!");*/
-        // Storage sqlStorage = Config.get().getStorage();
-        String uuid = request.getParameter("uuid");
-        if (uuid != null) {
-            response.getWriter().write(Config.get().getStorage().get(uuid).getUuid() + "|");
-            response.getWriter().write(Config.get().getStorage().get(uuid).getFullName());
-        } else {
-            /*response.getWriter().write("<!DOCTYPE html>\n" +
-                    "<html lang=\"en\">\n" +
-                    "<head>\n" +
-                    "\t<meta charset=\"UTF-8\">\n" +
-                    "\t<title>Resumes</title>\n" +
-                    "</head>\n" +
-                    "<body>\n" +
-                    "\t<!-- create table -->\n" +
-                    "\t\t\t<table>\n" +
-                    "\t\t\t\t<tr>\n" +
-                    "\t\t\t\t\t<th>UUID</th>\n" +
-                    "\t\t\t\t\t<th>Full_name</th>\n" +
-                    "\t\t\t\t</tr>");
-        }
-
-        for (Resume r : sqlStorage.getAllSorted()) {
-            response.getWriter().write("<tr>\n" +
-                    "\t\t\t\t\t<td>" + r.getUuid() + "</td>\n" +
-                    "\t\t\t\t\t<td>" + r.getFullName() + "</td>\n" +
-                    "\t\t\t\t</tr>");
-        }*/
-            response.getWriter().write("Hello!");
+        Writer writer = response.getWriter();
+        writer.write(
+                "<html>\n" +
+                        "<head>\n" +
+                        "<meta charset=\"UTF-8\">\n" +
+                        "<title>Resumes</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<section>\n" +
+                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
+                        "<tr>\n" +
+                        "<th>Имя</th>\n" +
+                        "<th>Email</th>\n" +
+                        "</tr>\n");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>\n" +
+                            "<td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
+                            "<td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
+                            "<tr>\n");
+            writer.write(
+                    "</table?\n" +
+                            "</section>\n" +
+                            "</body>\n" +
+                            "</html>\n");
         }
     }
 }
